d38d32bebf7f32ea63c78b831d5b5716ab71d5c6
Avvyxx <saberwatch@gmail.com>
2022-06-11 12:41:15 -0700

    replaced landing page with main layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
 import type { NextPage } from "next";
-import Head from "../components/Head";
-import styles from "../styles/Home.module.scss";
+import Main from "../components/pageLayouts/Main";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head title={"Unrugged"} />
-      <div className={styles.container}></div>
-    </>
-  );
+  return <Main></Main>;
 };
 
 export default Home;
