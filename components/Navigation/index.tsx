7709052fd3d58d631fe3cfce1352f9482f0189fb
Avvyxx <saberwatch@gmail.com>
2022-06-11 12:41:30 -0700

    formatting

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,18 +1,28 @@
 import Link from "next/link";
 import styles from "./index.module.scss";
 
+// prettier-ignore
 const Navigation = () => {
   return (
     <nav className={styles.container}>
       <h3 className={styles.unrugged}>UNRUGGED</h3>
       <div className={styles.center}>
-        <Link href={"/about"} passHref>
+        <Link 
+          href={"/about"}
+          passHref
+        >
           <a className={styles.link}>ABOUT US</a>
         </Link>
-        <Link href={"/mission"} passHref>
+        <Link 
+          href={"/mission"} 
+          passHref
+        >
           <a className={styles.link}>OUR MISSION</a>
         </Link>
-        <Link href={"/claim"} passHref>
+        <Link 
+          href={"/claim"} 
+          passHref
+        >
           <a className={styles.link}>CLAIM DAO</a>
         </Link>
       </div>
