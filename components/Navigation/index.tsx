f2b395910f35d56dcd593356f62f9a39c94d7781
Avvyxx <saberwatch@gmail.com>
2022-06-11 12:26:47 -0700

    Navigation component

---
+++ components/Navigation/index.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import styles from "./index.module.scss";
+
+const Navigation = () => {
+  return (
+    <nav className={styles.container}>
+      <h3 className={styles.unrugged}>UNRUGGED</h3>
+      <div className={styles.center}>
+        <Link href={"/about"} passHref>
+          <a className={styles.link}>ABOUT US</a>
+        </Link>
+        <Link href={"/mission"} passHref>
+          <a className={styles.link}>OUR MISSION</a>
+        </Link>
+        <Link href={"/claim"} passHref>
+          <a className={styles.link}>CLAIM DAO</a>
+        </Link>
+      </div>
+      <p className={styles.launch}>Launch app</p>
+    </nav>
+  );
+};
+
+export default Navigation;
