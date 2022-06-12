06320955ee6a885cb2c2a06b921c2c31f1e5328e
Avvyxx <saberwatch@gmail.com>
2022-06-11 18:32:02 -0700

    Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
+import Landing from "../components/Landing";
 import Main from "../components/pageLayouts/Main";
 
 const Home: NextPage = () => {
-  return <Main></Main>;
+  return (
+    <Main>
+      <Landing />
+    </Main>
+  );
 };
 
 export default Home;
