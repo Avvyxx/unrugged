7d88f5bc83e26734e78bbd3a1d51103d24c5ea2d
Avvyxx <saberwatch@gmail.com>
2022-06-12 20:09:45 -0700

    address component compatibility

--- components/Address/index.tsx
+++ components/Address/index.tsx
@@ -1,20 +1,40 @@
+import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 
 const Address = () => {
   const [token, setToken] = useState("");
 
+  const { route } = useRouter();
+
   useEffect(() => {
-    localStorage.setItem("unruggedtoken", token);
+    const temp = sessionStorage.getItem("unruggedToken");
+    if (temp) {
+      setToken(temp);
+    }
+  }, []);
+
+  useEffect(() => {
+    sessionStorage.setItem("unruggedToken", token);
   }, [token]);
 
   // prettier-ignore
   return (
-    <div className={styles.container}>
-      <p className={styles.top}>
-        Scan your Token Address to verify a crypto token.
-      </p>
+    <div className={clsx(styles.container, route === '/scanner' && styles.diff)}>
+      {route === "/" ? (
+        <p className={styles.top}>
+          Scan your Token Address to verify a crypto token.
+        </p>
+      ) : (
+        <div className={styles.connect}>
+          <p className={styles.top}>
+            Connect your wallet to proceed scanning your token.
+          </p>
+          <p className={styles.now}>Connect now</p>
+        </div>
+      )}
       <div className={styles.inputCon}>
         <input
           className={styles.input}
@@ -32,7 +52,11 @@ const Address = () => {
           <a className={styles.button}>Token Scanner</a>
         </Link>
       </div>
-      <p className={styles.bottom}>Claim your UnruggedDAO</p>
+      {route === "/" ? (
+        <p className={styles.bottom}>Claim your UnruggedDAO</p>
+      ) : (
+        <p className={clsx(styles.now, styles.show)}>Connect now</p>
+      )}
     </div>
   );
 };
