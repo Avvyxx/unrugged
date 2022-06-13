3cf1b48bbe241e24cef3d2e1beea913c37a6b030
Avvyxx <saberwatch@gmail.com>
2022-06-13 16:36:55 -0700

    Token input component reusability

--- components/Address/index.tsx
+++ components/Address/index.tsx
@@ -1,13 +1,9 @@
-import clsx from "clsx";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 
 const Address = () => {
   const [token, setToken] = useState("");
-
-  const { route } = useRouter();
 
   useEffect(() => {
     const temp = sessionStorage.getItem("unruggedToken");
@@ -22,41 +18,22 @@ const Address = () => {
 
   // prettier-ignore
   return (
-    <div className={clsx(styles.container, route === '/scanner' && styles.diff)}>
-      {route === "/" ? (
-        <p className={styles.top}>
-          Scan your Token Address to verify a crypto token.
-        </p>
-      ) : (
-        <div className={styles.connect}>
-          <p className={styles.top}>
-            Connect your wallet to proceed scanning your token.
-          </p>
-          <p className={styles.now}>Connect now</p>
-        </div>
-      )}
-      <div className={styles.inputCon}>
-        <input
-          className={styles.input}
-          type={"text"}
-          placeholder={"Token Address"}
-          value={token}
-          onChange={(event) => {
-            setToken(event.target.value);
-          }}
-        />
-        <Link 
-          href={"/scanner"} 
-          passHref
-        >
-          <a className={styles.button}>Token Scanner</a>
-        </Link>
-      </div>
-      {route === "/" ? (
-        <p className={styles.bottom}>Claim your UnruggedDAO</p>
-      ) : (
-        <p className={clsx(styles.now, styles.show)}>Connect now</p>
-      )}
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        type={"text"}
+        placeholder={"Token Address"}
+        value={token}
+        onChange={(event) => {
+          setToken(event.target.value);
+        }}
+      />
+      <Link 
+        href={"/scanner"} 
+        passHref
+      >
+        <a className={styles.button}>Token Scanner</a>
+      </Link>
     </div>
   );
 };
--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -15,7 +15,13 @@ const Landing = () => {
         <br />
         <b className={styles.notItalicized}>RECOVERY <em>PROTOCOL</em></b>
       </h1>
-      <Address />
+      <div className={styles.container}>
+        <p className={styles.top}>
+          Scan your Token Address to verify a crypto token.
+        </p>
+        <Address />
+        <p className={styles.bottom}>Claim your UnruggedDAO</p>
+      </div>
       <Data />
       <Information />
       <Mission />
