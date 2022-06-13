34272067f4f6b28cc40783a4b75c3923c4d430f5
Avvyxx <saberwatch@gmail.com>
2022-06-13 16:48:09 -0700

    scanner page address fix

--- components/Scan/index.tsx
+++ components/Scan/index.tsx
@@ -1,14 +1,16 @@
+import clsx from "clsx";
 import Address from "../Address";
 import styles from "./index.module.scss";
 
 const Scan = () => {
-  // prettier-ignore
   return (
     <>
+      {/* prettier-ignore */}
       <h1 className={styles.header}>
         <em><b className={styles.notItalicized}>TOKEN</b> SCANNER</em>
       </h1>
       <div className={styles.hold}>
+        {/* prettier-ignore */}
         <p className={styles.purpose}>
           <b className={styles.starter}>UnruggedDAO&apos;s purpose is simple:</b> to 
           help victims to some of the biggest cryptocurrency rug-pulls to get
@@ -16,7 +18,16 @@ const Scan = () => {
           detection and prevention on Ethereum and compatible networks.
         </p>
       </div>
-      <Address />
+      <div className={styles.container}>
+        <div className={styles.connect}>
+          <p className={styles.top}>
+            Connect your wallet to proceed scanning your token.
+          </p>
+          <p className={styles.bottom}>Connect now</p>
+        </div>
+        <Address />
+        <p className={clsx(styles.bottom, styles.res)}>Connect now</p>
+      </div>
     </>
   );
 };
