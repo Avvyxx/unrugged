db57cf0b58c60607388a1397aede728872d9ea59
Avvyxx <saberwatch@gmail.com>
2022-06-15 16:31:24 -0700

    scroll behavior

--- components/pageLayouts/Main/index.tsx
+++ components/pageLayouts/Main/index.tsx
@@ -1,20 +1,37 @@
 import clsx from "clsx";
 import { useRouter } from "next/router";
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import Footer from "../../Footer";
 import Head from "../../Head";
 import Navigation from "../../Navigation";
 import styles from "./index.module.scss";
 
 const Main: FC = ({ children }) => {
+  const [prevent, setPrevent] = useState(true);
   const { route } = useRouter();
+
+  const main = useRef<HTMLDivElement | null>(null);
+
+  const onScroll = () => {
+    if (main.current) {
+      const { scrollTop, scrollHeight, clientHeight } = main.current;
+      if (scrollTop + clientHeight === scrollHeight) {
+        setPrevent(false);
+      }
+    }
+  };
 
   return (
     <>
       <Head title={"Unrugged"} />
-      <div className={styles.container}>
+      <div className={clsx(styles.container, prevent && styles.prevent)}>
         <Navigation />
-        <main className={styles.main}>
+        {/* prettier-ignore */}
+        <main 
+          className={styles.main} 
+          ref={main} 
+          onScroll={onScroll}
+        >
           <div className={clsx(styles.dot, styles.first)}></div>
           <div className={clsx(styles.dot, styles.second)}></div>
           <div className={clsx(styles.dot, styles.third)}></div>
