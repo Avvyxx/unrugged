41ebe933f652f6333b820745e171a4e3fc2ee356
Avvyxx <saberwatch@gmail.com>
2022-06-15 19:27:10 -0700

    fix

--- components/pageLayouts/Main/index.tsx
+++ components/pageLayouts/Main/index.tsx
@@ -1,36 +1,22 @@
 import clsx from "clsx";
 import { useRouter } from "next/router";
-import { FC, useRef, useState } from "react";
+import { FC } from "react";
 import Footer from "../../Footer";
 import Head from "../../Head";
 import Navigation from "../../Navigation";
 import styles from "./index.module.scss";
 
 const Main: FC = ({ children }) => {
-  const [prevent, setPrevent] = useState(true);
   const { route } = useRouter();
-
-  const main = useRef<HTMLDivElement | null>(null);
-
-  const onScroll = () => {
-    if (main.current) {
-      const { scrollTop, scrollHeight, clientHeight } = main.current;
-      if (scrollTop + clientHeight === scrollHeight) {
-        setPrevent(false);
-      }
-    }
-  };
 
   return (
     <>
       <Head title={"Unrugged"} />
-      <div className={clsx(styles.container, prevent && styles.prevent)}>
+      <div className={styles.container}>
         <Navigation />
         {/* prettier-ignore */}
         <main 
           className={styles.main} 
-          ref={main} 
-          onScroll={onScroll}
         >
           <div className={clsx(styles.dot, styles.first)}></div>
           <div className={clsx(styles.dot, styles.second)}></div>
